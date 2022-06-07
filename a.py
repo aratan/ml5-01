78d9f1e82473c227e5d01e09353cfe169031ffa0
Víctor <systemdeveloper868@gmail.com>
2022-06-07 13:16:38 +0200

    filtros

--- a.py
+++ a.py
@@ -19,6 +19,22 @@ print(sales['Age'].median()) #mediana
 print(sales['Customer spendings'].mean())
 print(sales['Customer spendings'].median())
 
+#filtro elimina las ventas menos de 10€
+#mixto sales = sales.loc[~((sales['Customer spendings'] == 0) | (sales['Age'] == 0))]
+sales = sales[sales['Customer spendings'] < 10]
+print(sales)
+
+#elimina las filas duplicadas 
+sales = sales.drop_duplicates()
+print(sales)
+
+#Elimina vacias
+sales = sales.dropna()
+
+print(sales)
+
 # graficos
 plt.bar(sales['Country'],sales['Customer spendings'])
 plt.show()
+
+
